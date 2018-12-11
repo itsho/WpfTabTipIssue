93c562d121a164e2a726ce3038fd0904b900c699
Itsho <itsho.itsho@gmail.com>
2018-12-11 15:45:50 +0200

    First commit

---
+++ WpfTabTipIssue/MainWindow.xaml.cs
@@ -0,0 +1,26 @@
+﻿using System.IO;
+using System.Windows;
+
+namespace WpfTabTipIssue
+{
+	public partial class MainWindow : Window
+	{
+		public MainWindow()
+		{
+			InitializeComponent();
+		}
+
+		private void ButtonBase_OnClick(object sender, RoutedEventArgs e)
+		{
+			var tabTip = @"C:\Program Files\Common Files\Microsoft Shared\ink\TabTip.exe";
+			if (File.Exists(tabTip))
+			{
+				System.Diagnostics.Process.Start(tabTip);
+			}
+			else
+			{
+				MessageBox.Show("Unable to find TabTip");
+			}
+		}
+	}
+}
